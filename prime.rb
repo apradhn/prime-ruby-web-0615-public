afb4cc7f04171402ac76b0c433245e489d930f06
apradhn <abhijit.pradhan6@gmail.com>
2015-06-04 12:09:42 -0400

    all done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  # check whether any int x from 2 to num-1 evenly divides num
+  answer = true
+  2.upto(Math.sqrt(num)) do |i|
+    if num % i == 0
+      answer = false
+    end
+  end
+  answer
+end
